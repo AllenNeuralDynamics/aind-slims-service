4791a30ab18108177f2598e2261d757e4db67bc8
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2025-07-23 09:37:37 -0700

    Feat aws manager (#23)
    * feat: aws manager
    
    * docs: updates readme

--- aind-slims-service-server/src/aind_slims_service_server/configs.py
+++ aind-slims-service-server/src/aind_slims_service_server/configs.py
@@ -1,13 +1,11 @@
 """Module for settings to connect to SLIMS backend"""
 
-from aind_settings_utils.aws import (
-    ParameterStoreAppBaseSettings,
-)
+from aind_settings_utils.aws import SecretsManagerBaseSettings
 from pydantic import Field, SecretStr
 from pydantic_settings import SettingsConfigDict
 
 
-class Settings(ParameterStoreAppBaseSettings):
+class Settings(SecretsManagerBaseSettings):
     """Settings for connecting to SLIMS Database."""
 
     username: str = Field(..., description="User name")
